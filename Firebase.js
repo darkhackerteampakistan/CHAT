2bfde2f4d575ec8738631baddddb3a76991b8c2c
darkhackerteampakistan <darkhackerteampakistan@gmail.com>
2026-08-04 17:50:20 +0600

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // 🔥 তোমার Firebase project এর config এখানে বসাও
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCSZqZ-k6pLJoXa06xh1RIZdoPnKYSSaTQ",
+  authDomain: "chat-with-hacker-8ea76.firebaseapp.com",
+  projectId: "chat-with-hacker-8ea76",
+  storageBucket: "chat-with-hacker-8ea76.firebasestorage.app",
+  messagingSenderId: "84225420811",
+  appId: "1:84225420811:web:8e6733d55a7ea195fca10e",
+  measurementId: "G-C9C6V1CVZY"
 };
 
 const app = initializeApp(firebaseConfig);
